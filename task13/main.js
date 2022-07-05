3c8a31109513758dd569b12d444b450b33406777
gowrimurugan <gowrimurugan1323@gmail.com>
2022-07-05 16:57:42 +0530

    task13

---
+++ task13/main.js
@@ -0,0 +1,8 @@
+let num1 = 80;
+let r = num1%7;
+
+if(r==0){
+    console.log("Number" +num1+" is multiple of 7 ")
+}else{
+    console.log("Number" +num1+" is not multiple of 7")
+} 
---
+++ task14/main.js
@@ -0,0 +1,7 @@
+let num1 = 26;
+
+if(num1 > 10){
+    console.log(num1+" is greater than 10")
+}else{
+    console.log(num1+" is less than 10")
+} 
